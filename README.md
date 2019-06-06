c7347719a15a6c79fe91635040a8efcfda3431ba
David Urbansky <davidurbansky@gmail.com>
2019-06-06 16:04:50 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # javascript-search
 Javascript Search - Serverless Superfast Search
+
+A super fast search for one-pager with a lot of information, for example api documentations. The script is licensed under MIT, dependency free, and supported by the team at [Site Search 360](https://www.sitesearch360.com).
+
+Try it yourself using the [demo](https://htmlpreview.github.io/?https://github.com/ddsky/javascript-search/blob/master/html/example.html)
+![Javascript Search in action](media/js-search.gif "Javascript Search in action")
+
+## Install
+Include the `script` and the `css` and then initialize the script to an element that triggers the search box (e.g. an element with the id "triggerBox")
+
+```html
+<link href="css/js-search.css" rel="stylesheet" type="text/css"/>
+<script src="js/js-search.js"></script>
+<script>
+    // initialize the javascript
+    JsSearch.init('triggerBox', options);
+
+    // enable Ctrl+F shortcut
+    JsSearch.enableShortcuts();
+</script>
+```
+
+## Configuration
+You can pass the following `options` to the `JsSearch.init`.
+```js
+{
+    // the placeholder for the search box
+    placeholder: 'Search here'
+}
+```
+
+## Indexing
+When calling `JsSearch.init` all elements with the class `jss-doc` will be indexed. A document can consist of the following fields
+
+- `jss-title`: The title of the document.
+- `jss-content`: The content of the document. If the attribute is not set, the text content of the node will be indexed as the content.
+- `jss-group`: Optionally, a group in which the document belongs to.
+- `jss-meta`: Optionally, a short description that is shown in the search results.
+- `jss-event`: The code to execute when a user chooses a result.
+
+A document can be tagged in html the following way:
+```html
+<!-- minimal document -->
+<p class="jss-doc" jss-title="Document 1">This is the content of the document.</p>
+
+<!-- detailed document-->
+<p class="jss-doc" jss-title="Document 1" jss-group="My Group" jss-meta="A short description" jss-event="alert(1)">Long content.</p>
+```
---
+++ js/js-search.js
@@ -0,0 +1,285 @@
+/**
+ * A simple search for modern one pagers, only in browser. No costly requests to servers necessary.
+ * @licence MIT
+ * @author David Urbansky (https://davidurbansky.com/)
+ * @see https://github.com/ddsky/javascript-search
+ */
+var JsSearch = function JsSearch() {
+    var index = {};
+    var documents = {};
+    var selectedIndex = 0;
+    var selectedDocument = undefined;
+
+    function showSearchLayer() {
+        document.getElementById('jss-search').style.display = 'block';
+        document.getElementById('jss-search-layer').style.display = 'block';
+        document.getElementById('jss-search-box').focus();
+
+    }
+
+    function hideSearchLayer() {
+        document.getElementById('jss-search').style.display = 'none';
+        document.getElementById('jss-search-layer').style.display = 'none';
+    }
+
+    function showResults(docIds) {
+        document.getElementById('jss-search-results').style.display = 'none';
+        document.getElementById('jss-search-results').innerHTML = '';
+        if (!docIds) {
+            return;
+        }
+        document.getElementById('jss-search-results').style.display = 'block';
+        docIds.forEach(function (docId) {
+            var doc = documents[docId];
+            var docNode = document.createElement('div');
+            docNode.className = 'jss-search-result';
+            docNode.innerHTML = '<span class="title">' + doc.title + '</span>';
+            if (doc.group) {
+                docNode.innerHTML += '<span class="group"> — ' + doc.group + '</span>';
+            }
+            if (doc.meta) {
+                docNode.innerHTML += '<p class="meta">' + doc.meta + '</p>';
+            }
+            docNode.onclick = function () {
+                eval(doc.event)
+            };
+
+            document.getElementById('jss-search-results').appendChild(docNode);
+        });
+
+        // select the right one
+        var searchResults = document.getElementsByClassName('jss-search-result');
+        selectedIndex = selectedIndex % searchResults.length;
+        if (selectedIndex < 0) {
+            selectedIndex = searchResults.length - 1;
+        }
+
+        // remember the selected document (so that when enter is pressed we know what function to execute)
+        var list = Array.from(docIds);
+        selectedDocument = documents[list[selectedIndex]];
+
+        for (var i = 0; i < searchResults.length; i++) {
+            searchResults[i].className = 'jss-search-result';
+        }
+        var selectedDomElement = document.getElementsByClassName('jss-search-result').item(selectedIndex);
+        selectedDomElement.className = 'jss-search-result active';
+        selectedDomElement.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+    }
+
+    function intersection(setA, setB) {
+        var _intersection = new Set();
+        setB.forEach(function (elem) {
+            if (setA.has(elem)) {
+                _intersection.add(elem);
+            }
+        });
+        return _intersection;
+    }
+
+    /**
+     * Calculate left-edge n-grams from the string's words between n1 and n2 characters.
+     * @param string The input string.
+     * @param n1 The minimum n-gram length.
+     * @param n2 The maximum n-gram length.
+     * @returns {Set} Set of n-grams.
+     */
+    function calculateAllEdgeCharNgrams(string, n1, n2) {
+        var nGrams = new Set();
+        var parts = string.split(' ');
+        for (var i = 0; i < parts.length; i++) {
+            var p = parts[i];
+            for (var n = n1; n <= n2; n++) {
+                calculateCharEdgeNGrams(p, n, true).forEach(function (value) {
+                    nGrams.add(value);
+                });
+            }
+        }
+
+        return nGrams;
+    }
+
+    /**
+     * Calculate left-edge n-grams from the string with n characters.
+     * @param string The input string.
+     * @param n The n-gram length.
+     * @param mustHitLeftEdge Whether the n-gram must hit the left edge.
+     * @returns {Set} Set of n-grams.
+     */
+    function calculateCharEdgeNGrams(string, n, mustHitLeftEdge) {
+        var nGrams = new Set();
+
+        var length = string.length;
+        if (length < n) {
+            return nGrams;
+        }
+
+        for (var i = 0; i <= length - n; i++) {
+            var nGram = '';
+            for (var j = i; j < i + n; j++) {
+                nGram += string.charAt(j);
+            }
+            nGrams.add(nGram);
+
+            if (i === 0 && mustHitLeftEdge) {
+                break;
+            }
+        }
+
+        return nGrams;
+    }
+
+    function deepExtend(out) {
+        out = out || {};
+
+        for (var i = 1; i < arguments.length; i++) {
+            var obj = arguments[i];
+
+            if (!obj) {
+                continue;
+            }
+
+            for (var key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    if (typeof obj[key] === 'object') {
+                        out[key] = deepExtend(out[key], obj[key]);
+                    } else if (!out[key]) {
+                        out[key] = obj[key];
+                    }
+                }
+            }
+        }
+
+        return out;
+    }
+
+    // return an object, through closure all methods keep bound to returned object
+    return {
+        init: function (triggerSearchBoxId, options) {
+
+            // get default options
+            if (!options) {
+                options = {};
+            }
+            deepExtend(options, {
+                placeholder: 'Quick Search',
+            });
+
+            // create necessary HTML
+            var backgroundLayer = document.createElement('div');
+            backgroundLayer.id = 'jss-search-layer';
+            document.body.appendChild(backgroundLayer);
+            var searchLayer = document.createElement('div');
+            searchLayer.id = 'jss-search';
+            searchLayer.innerHTML = '<input id="jss-search-box" type="text" placeholder="' + options.placeholder + '" /><div id="jss-search-results"></div>';
+            document.body.appendChild(searchLayer);
+
+            var self = this;
+            document.getElementById('jss-search-layer').onclick = hideSearchLayer;
+            document.getElementById(triggerSearchBoxId).onfocus = showSearchLayer;
+            document.getElementById('jss-search-box').onkeyup = function (event) {
+                if (event.keyCode === 27) {
+                    hideSearchLayer();
+                    return;
+                } else if (event.keyCode === 38) {
+                    selectedIndex--;
+                    event.preventDefault();
+                } else if (event.keyCode === 40) {
+                    selectedIndex++;
+                    event.preventDefault();
+                } else if (event.keyCode === 13 && selectedDocument) {
+                    eval(selectedDocument.event);
+                    event.preventDefault();
+                }
+                self.search(this.value);
+            };
+
+            this.indexDocuments();
+        },
+        indexDocuments: function () {
+            var docs = document.getElementsByClassName('jss-doc');
+            for (var i = 0; i < docs.length; i++) {
+                var doc = docs[i];
+                var title = doc.getAttribute('jss-title');
+                var group = doc.getAttribute('jss-group');
+                var meta = doc.getAttribute('jss-meta');
+                var content = doc.getAttribute('jss-content');
+                if (!content) {
+                    content = doc.textContent;
+                }
+                var id = doc.getAttribute('jss-id');
+                if (!id) {
+                    id = 'jss-id-' + i;
+                }
+                var jssEvent = doc.getAttribute('jss-event');
+                var documentObject = {
+                    id: id,
+                    title: title,
+                    group: group,
+                    meta: meta,
+                    content: content,
+                    event: jssEvent
+                };
+
+                // title and group will be edge-ngrammized
+                var words = (title + ' ' + group + ' ' + meta).split(' ');
+                for (var j = 0; j < words.length; j++) {
+                    var word = words[j].toLowerCase();
+
+                    var ngrams = calculateAllEdgeCharNgrams(word, 1, 12);
+
+                    ngrams.forEach(function (ngram) {
+                        var docIds = index[ngram];
+                        if (!docIds) {
+                            docIds = new Set();
+                            index[ngram] = docIds;
+                        }
+                        docIds.add(id);
+                    });
+                }
+
+                // content will only added by words
+                words = content.split(' ');
+                for (j = 0; j < words.length; j++) {
+                    word = words[j].toLowerCase();
+
+                    var docIds = index[word];
+                    if (!docIds) {
+                        docIds = new Set();
+                        index[word] = docIds;
+                    }
+                    docIds.add(id);
+                }
+
+                documents[id] = documentObject;
+            }
+
+            // console.log(index);
+        },
+        search: function (query) {
+            var tsStart = new Date().getTime();
+            var parts = query.toLowerCase().split(' ');
+            var idsFound = undefined;
+            for (var i = 0; i < parts.length; i++) {
+                var tempIds = index[parts[i]];
+
+                if (idsFound === undefined) {
+                    idsFound = tempIds;
+                } else if (tempIds !== undefined) {
+                    // idsFound = idsFound.filter(v => tempIds.includes(v));
+                    idsFound = intersection(idsFound, tempIds);
+                }
+            }
+
+            showResults(idsFound);
+            // console.log('searched in ' + (new Date().getTime() - tsStart) + 'ms');
+        },
+        enableShortcuts: function () {
+            window.onkeydown = function (event) {
+                if (event.ctrlKey && event.keyCode === 70) {
+                    event.preventDefault();
+                    showSearchLayer();
+                }
+            };
+        }
+    }
+}();
